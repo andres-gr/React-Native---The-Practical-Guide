5f4a0660f3f95ba647d520d74d7db7443a5edaed
Andres Garcia <andres.garcia@bmtask.com>
2018-01-25 00:18:56 -0600

    section 7, lecture 101 complete

--- src/components/UI/RaisedButton.js
+++ src/components/UI/RaisedButton.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Platform, StyleSheet, Text, TouchableNativeFeedback, TouchableOpacity, View } from 'react-native'
 
 const styles = StyleSheet.create({
     button: {
@@ -12,25 +12,38 @@ const styles = StyleSheet.create({
     }
 })
 
-const RaisedButton = props => (
-    <TouchableOpacity onPress={ props.onPress }>
-        <View style={ [styles.button, { backgroundColor: props.color }] }>
-            <Text>{ props.children }</Text>
+class RaisedButton extends PureComponent {
+    static propTypes = {
+        onPress     : PropTypes.func.isRequired,
+        children    : PropTypes.oneOfType([
+            PropTypes.object,
+            PropTypes.string
+        ]).isRequired,
+        color: PropTypes.string
+    }
+
+    static defaultProps = {
+        color: null
+    }
+    getButtonContent = () => (
+        <View style={ [styles.button, { backgroundColor: this.props.color }] }>
+            <Text>{ this.props.children }</Text>
         </View>
-    </TouchableOpacity>
-)
-
-RaisedButton.propTypes = {
-    onPress     : PropTypes.func.isRequired,
-    children    : PropTypes.oneOfType([
-        PropTypes.object,
-        PropTypes.string
-    ]).isRequired,
-    color: PropTypes.string
-}
-
-RaisedButton.defaultProps = {
-    color: null
+    )
+    render () {
+        if (Platform.OS === 'android') {
+            return (
+                <TouchableNativeFeedback onPress={ this.props.onPress }>
+                    { this.getButtonContent() }
+                </TouchableNativeFeedback>
+            )
+        }
+        return (
+            <TouchableOpacity onPress={ this.props.onPress }>
+                { this.getButtonContent() }
+            </TouchableOpacity>
+        )
+    }
 }
 
 export default RaisedButton
