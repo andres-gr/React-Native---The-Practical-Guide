a546955381119c6e7a1e572a27b531ea3e14c97a
Andres Garcia <andres.garcia@bmtask.com>
2018-01-28 23:10:04 -0600

    section 7, lecture 114

--- src/decorators/withCurrentPlaces.js
+++ src/decorators/withCurrentPlaces.js
@@ -2,7 +2,7 @@ import { graphql } from 'react-apollo'
 import GET_CURRENT_PLACES from '../api/graphql/state/root/places.graphql'
 
 const withCurrentPlaces = graphql(GET_CURRENT_PLACES, {
-    props: ({ data: { places } }) => ({ places })
+    props: ({ data: { loading, places } }) => ({ loading, places })
 })
 
 export default withCurrentPlaces
--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,9 +1,35 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { View } from 'react-native'
+import { Animated, Text, TouchableWithoutFeedback, View } from 'react-native'
+import glamFactory from '../../utils/styles/glamFactory'
 import List from '../../components/List/List'
 import withCurrentPlaces from '../../decorators/withCurrentPlaces'
 import sideDrawerToggle from '../../utils/helpers/sideDrawerToggle'
+
+const { View: AnimView } = Animated
+
+const GlamFindPlaceContainer = glamFactory(View, 'GlamFindPlaceContainer', {}, ({ loaded }) => ({
+    alignItems     : loaded ? 'flex-start' : 'center',
+    flex           : 1,
+    justifyContent : loaded ? 'flex-start' : 'center'
+}))
+
+const GlamSearchButton = glamFactory(View, 'GlamSearchButton', {
+    borderColor  : 'orange',
+    borderRadius : 50,
+    borderWidth  : 3,
+    padding      : 20
+})
+
+const GlamSearchText = glamFactory(Text, 'GlamSearchText', {
+    color      : 'orange',
+    fontSize   : 26,
+    fontWeight : 'bold'
+})
+
+const GlamAnimated = glamFactory(AnimView, 'GlamAnimated')
+
+GlamAnimated.propsAreStyleOverrides = true
 
 @withCurrentPlaces
 class FindPlaceScreen extends PureComponent {
@@ -14,6 +40,10 @@ class FindPlaceScreen extends PureComponent {
     constructor (props) {
         super(props)
         props.navigator.addOnNavigatorEvent(sideDrawerToggle.bind(this, { side: 'left' }))
+    }
+    state = {
+        loaded         : false,
+        removeAnimated : new Animated.Value(1)
     }
     _handleItemPress = key => {
         const selectedPlace = this.props.places.find(place => place.key === key)
@@ -29,14 +59,51 @@ class FindPlaceScreen extends PureComponent {
             }
         })
     }
+    _handleSearchPlaces = () => {
+        Animated.timing(this.state.removeAnimated, {
+            duration        : 500,
+            toValue         : 0,
+            useNativeDriver : true
+        }).start()
+    }
+    opacityAnimatedProps = {
+        opacity   : this.state.removeAnimated,
+        transform : [
+            {
+                scale: this.state.removeAnimated.interpolate({
+                    inputRange  : [0, 1],
+                    outputRange : [12, 1]
+                })
+            }
+        ]
+    }
     render () {
         return (
-            <View>
-                <List
-                    pressEvent={ this._handleItemPress }
-                    places={ this.props.places }
-                />
-            </View>
+            <GlamFindPlaceContainer
+                loaded={ this.state.loaded }
+            >
+                { this.state.loaded
+                    ? (
+                        <List
+                            pressEvent={ this._handleItemPress }
+                            places={ this.props.places }
+                        />
+                    )
+                    : (
+                        <GlamAnimated
+                            { ...this.opacityAnimatedProps }
+                        >
+                            <TouchableWithoutFeedback
+                                onPress={ this._handleSearchPlaces }
+                            >
+                                <GlamSearchButton>
+                                    <GlamSearchText>Find Places</GlamSearchText>
+                                </GlamSearchButton>
+                            </TouchableWithoutFeedback>
+                        </GlamAnimated>
+                    )
+                }
+            </GlamFindPlaceContainer>
         )
     }
 }
