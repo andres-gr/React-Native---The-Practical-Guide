7d13eba7422eb83aa6ca917681a70f6cf0cbe311
Andres Garcia <andres.garcia@bmtask.com>
2018-01-28 20:13:38 -0600

    section 7, lecture 108 complete

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ImageBackground, View } from 'react-native'
+import { Dimensions, ImageBackground, View } from 'react-native'
 import startMainTabs from '../MainTabs/startMainTabs'
 import glamFactory from '../../utils/styles/glamFactory'
 import DefaultInput from '../../components/UI/DefaultInput'
@@ -31,11 +31,31 @@ const inputStyles = {
 
 const GlamEmailContainer = glamFactory(DefaultInput, 'GlamEmailContainer', inputStyles)
 
+const GlamPasswordContainer = glamFactory(View, 'GlamPasswordContainer', {}, ({ windowHeight }) => ({
+    flexDirection  : windowHeight > 500 ? 'column' : 'row',
+    justifyContent : windowHeight > 500 ? 'flex-start' : 'space-between'
+}))
+
+const GlamPasswordWrapper = glamFactory(View, 'GlamPasswordWrapper', {}, ({ windowHeight }) => ({
+    width: windowHeight > 500 ? '100%' : '45%'
+}))
+
 const GlamPasswordInput = glamFactory(DefaultInput, 'GlamPasswordInput', inputStyles)
 
 const GlamConfirmInput = glamFactory(DefaultInput, 'GlamConfirmInput', inputStyles)
 
 class AuthScreen extends Component {
+    constructor (props) {
+        super(props)
+        Dimensions.addEventListener('change', this._handleOrientationChange)
+    }
+    state = {
+        windowHeight: Dimensions.get('window').height
+    }
+    _handleOrientationChange = dimensions => {
+        const { window: { height } } = dimensions
+        this.setState({ windowHeight: height })
+    }
     _handleSwitchLogin = () => {
         alert('Cosa')
     }
@@ -46,9 +66,14 @@ class AuthScreen extends Component {
         return (
             <GlamImageBackground source={ backgroundImage }>
                 <GlamAuthContainer>
-                    <MainText>
-                        <HeadingText>Please Log In</HeadingText>
-                    </MainText>
+                    { this.state.windowHeight > 500
+                        ? (
+                            <MainText>
+                                <HeadingText>Please Log In</HeadingText>
+                            </MainText>
+                        )
+                        : null
+                    }
                     <RaisedButton
                         color="#29AAF4"
                         onPress={ this._handleSwitchLogin }
@@ -59,12 +84,24 @@ class AuthScreen extends Component {
                         <GlamEmailContainer
                             placeholder="Your email Address"
                         />
-                        <GlamPasswordInput
-                            placeholder="Password"
-                        />
-                        <GlamConfirmInput
-                            placeholder="Confirm Password"
-                        />
+                        <GlamPasswordContainer
+                            windowHeight={ this.state.windowHeight }
+                        >
+                            <GlamPasswordWrapper
+                                windowHeight={ this.state.windowHeight }
+                            >
+                                <GlamPasswordInput
+                                    placeholder="Password"
+                                />
+                            </GlamPasswordWrapper>
+                            <GlamPasswordWrapper
+                                windowHeight={ this.state.windowHeight }
+                            >
+                                <GlamConfirmInput
+                                    placeholder="Confirm Password"
+                                />
+                            </GlamPasswordWrapper>
+                        </GlamPasswordContainer>
                     </GlamInputContainer>
                     <RaisedButton
                         color="#29AAF4"
